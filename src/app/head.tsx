81a85fdf920ff313472d425c1690fe1b5a67a0e1
Mateus Borges <spekytro68@gmail.com>
2023-05-07 01:43:55 -0300

    ajustes

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>GitHub Repo</title>
+      </Head>
+      <p>Author: Mateus Borges</p>
+    </div>
+  );
+}
+
+export default IndexPage;
